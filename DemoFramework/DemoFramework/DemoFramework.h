bd580ed04fd90714cec620eb68f5825f0c763fcd
Dhara Bhuva <dhara.bhuva@plusinfosys.com>
2024-06-07 07:34:22 +0530

    initial

---
+++ DemoFramework/DemoFramework/DemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Dhara Bhuva on 06/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
